36b3defed57e987ceca9309d2d18115e66902274
basuabhirup <basu.abhirup@yahoo.com>
2021-08-13 00:00:45 +0530

    Set sliders for volume and playbackRate control.

--- scripts.js
+++ scripts.js
@@ -7,19 +7,26 @@ const toggle = player.querySelector(".toggle");
 const skipButtons = player.querySelectorAll("[data-skip]");
 const ranges = player.querySelectorAll(".player__slider");
 
+
+
 // Build out functions
-const togglePlay = () => {
+function togglePlay() {
   video.paused ? video.play() : video.pause() ;
 }
 
-const updateButton = () => {
-  video.paused ? toggle.textContent = "►" : toggle.textContent = "||";
+function updateButton() {
+  video.paused ? toggle.textContent = "►" : toggle.textContent = "❚ ❚";
 }
 
-function skip () {
+function skip() {
   let skippingTime = parseFloat(this.dataset.skip);
   video.currentTime += skippingTime;
 }
+
+function handleRange() {
+  video[this.name] = this.value;
+}
+
 
 
 // Hook up the event listeners
@@ -27,6 +34,14 @@ video.addEventListener("click", togglePlay);
 video.addEventListener("play", updateButton);
 video.addEventListener("pause", updateButton);
 toggle.addEventListener("click", togglePlay);
+
 for (let i = 0; i < skipButtons.length; i++) {
   skipButtons[i].addEventListener("click", skip);
+}
+
+for (let i = 0; i < ranges.length; i++) {
+  ranges[i].addEventListener("change", handleRange);
+}
+for (let i = 0; i < ranges.length; i++) {
+  ranges[i].addEventListener("mousemove", handleRange);
 }
